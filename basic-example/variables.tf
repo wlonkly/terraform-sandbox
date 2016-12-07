8907dc537c73045399d2f2ecfa74a5405ad129b9
Rich Lafferty <rich@lafferty.ca>
2016-12-07 16:21:06 -0500

    add domain variable to example

--- basic-example/variables.tf
+++ basic-example/variables.tf
@@ -4,10 +4,4 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "amis" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-  }
-}
+variable "domain"
