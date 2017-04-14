a77ef24dff349ff1c50973d5332ec1571751cb92
Rich Lafferty <rich@lafferty.ca>
2017-04-14 18:19:24 -0400

    catch up outstanding changes

--- basic-example/example.tf
+++ basic-example/example.tf
@@ -5,16 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
-  
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
-  }
-}
-
-resource "aws_eip" "ip" {
-    instance = "${aws_instance.example.id}"
 }
 
 output "ip" {
-    value = "${aws_eip.ip.public_ip}"
+    value = "${aws_instance.example.public_ip}"
 }
