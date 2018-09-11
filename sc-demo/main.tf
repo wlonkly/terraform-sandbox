5fa9c0644c1e5a9eb95579c35e4ef70baa483cdd
Rich Lafferty <rlafferty@pagerduty.com>
2018-09-11 10:49:53 -0400

    Basic TF demo for SC presentation

---
+++ sc-demo/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "sc-example" {
+  count           = "3"
+  ami             = "ami-9526abf1"
+  instance_type   = "t2.micro"
+  key_name        = "rich id_rsa"
+  subnet_id       = "subnet-9ac70ef3"
+  security_groups = ["sg-609c0809"]
+
+  tags = {
+    terraform = "true"
+  }
+}
+
+output "ip" {
+    value = "${aws_instance.sc-example.*.public_ip}"
+}
---
+++ sc-demo/provider.tf
@@ -0,0 +1,6 @@
+
+provider "aws" {
+  region  = "ca-central-1"
+  profile = "personal"
+}
+
