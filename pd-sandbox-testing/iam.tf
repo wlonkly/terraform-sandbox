353b32729590e47d666d2998dbf2d67bf67f3097
Rich Lafferty <rlafferty@pagerduty.com>
2020-02-08 11:01:10 -0500

    IAM for SSM

---
+++ pd-sandbox-testing/iam.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "instance-role" {
+  name = "rlafferty-instance-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name  = "rlafferty-instance-role"
+    owner = "rlafferty"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "role-attach" {
+  role       = aws_iam_role.instance-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "rlafferty-test-profile"
+  role = aws_iam_role.instance-role.name
+}
+
