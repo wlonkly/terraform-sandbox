98999047b617d6a271b6c1dca0306f669f68df41
Rich Lafferty <rlafferty@pagerduty.com>
2020-10-06 00:00:49 -0400

    Working cluster

--- pd-sandbox-eks/eks.tf
+++ pd-sandbox-eks/eks.tf
@@ -1,5 +1,13 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
 module "my-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
+  source          = "github.com/wlonkly/terraform-aws-eks"
   cluster_name    = "rlafferty-eks-cluster"
   cluster_version = "1.17"
   subnets         = aws_subnet.subnet[*].id
@@ -13,4 +21,12 @@ module "my-cluster" {
       asg_max_size  = 3
     }
   ]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
 }
